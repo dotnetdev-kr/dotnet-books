d313ff4f76f539f0115332036155e73c982648b6
dimohy <dimohy01@gmail.com>
2021-03-12 23:51:19 +0900

    'Anchor, Dock을 이용한 컨트롤 레이아웃 디자인' 초안
    템플릿용으로 'Anchor, Dock을 이용한 컨트롤 레이아웃 디자인'을 작성함

--- docs/for-win-developers/Windows-Forms/1.DesignControlLayout/sample/sample/Program.cs
+++ docs/for-win-developers/Windows-Forms/1.DesignControlLayout/sample/sample/Program.cs
@@ -17,7 +17,10 @@ namespace sample
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            //Application.Run(new Form1());
+            //Application.Run(new Form2());
+            //Application.Run(new Form3());
+            Application.Run(new Form5());
         }
     }
 }
